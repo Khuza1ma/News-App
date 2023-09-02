c66807095b3045e777d33d359efbd29de490ee5b
Khuza1ma <khuzaimanrko@gmail.com>
2023-09-02 22:21:23 +0530

    Uploading App Files

---
+++ lib/features/daily_news/domain/usecases/get_article.dart
@@ -0,0 +1,17 @@
+import 'package:news_app/core/resources/data_state.dart';
+import 'package:news_app/core/usecase/usecase.dart';
+import 'package:news_app/features/daily_news/domain/entities/article.dart';
+import 'package:news_app/features/daily_news/domain/repository/article_repository.dart';
+
+class GetArticleUseCase implements UseCase<DataState<List<ArticleEntity>>,void>{
+  
+  final ArticleRepository _articleRepository;
+
+  GetArticleUseCase(this._articleRepository);
+  
+  @override
+  Future<DataState<List<ArticleEntity>>> call({void params}) {
+    return _articleRepository.getNewsArticles();
+  }
+  
+}
